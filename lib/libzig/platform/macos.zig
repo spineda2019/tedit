95b67599e875420e749859510e4836abfa407d16
spineda2019 <spineda.wpi.alum@gmail.com>
2026-05-11 18:37:52 -0400

    stub mac

---
+++ lib/libzig/platform/macos.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+pub const file_handle_t = std.posix.fd_t;
+
+const Error = error{};
+
+pub fn enterCookedMode() Error!void {}
+
+pub fn enterRawMode() Error!void {}
+
+pub fn read(file: file_handle_t) Error!u8 {
+    _ = file;
+    return 0;
+}
+
+pub fn write(file: file_handle_t, char: u8) Error!void {
+    _ = file;
+    _ = char;
+}
+
+pub fn clear_screen() Error!void {}
