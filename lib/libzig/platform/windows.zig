c9db993ff87cee4fee56c7dddc3f28461ae7abb1
spineda2019 <spineda.wpi.alum@gmail.com>
2026-05-11 18:47:20 -0400

    windows read proto

--- lib/libzig/platform/windows.zig
+++ lib/libzig/platform/windows.zig
@@ -2,15 +2,34 @@ const std = @import("std");
 
 pub const file_handle_t = std.os.windows.HANDLE;
 
-const Error = error{};
+const Error = error{
+    ReadFailure,
+};
 
 pub fn enterCookedMode() Error!void {}
 
 pub fn enterRawMode() Error!void {}
 
 pub fn read(file: file_handle_t) Error!u8 {
-    _ = file;
-    return 0;
+    var char = [1]u8{0};
+    var iosb: std.os.windows.IO_STATUS_BLOCK = undefined;
+    const result = std.os.windows.ntdll.NtReadFile(
+        file,
+        null,
+        null,
+        null,
+        &iosb,
+        &char,
+        char.len,
+        null,
+        null,
+    );
+
+    if (result == .SUCCESS) {
+        return char[0];
+    } else {
+        return Error.ReadFailure;
+    }
 }
 
 pub fn write(file: file_handle_t, char: u8) Error!void {
