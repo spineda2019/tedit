9f9cbed27dcc446a041a560c12b6d92a3279131e
spineda2019 <spineda.wpi.alum@gmail.com>
2026-05-16 16:29:47 -0400

    Array

--- lib/libcpp/include/Array.hpp
+++ lib/libcpp/include/Array.hpp
@@ -1,28 +1,22 @@
-#ifndef SRC_INCLUDE_ARRAY_HPP_
-#define SRC_INCLUDE_ARRAY_HPP_
+#ifndef LIB_LIBCPP_INCLUDE_ARRAY_HPP_
+#define LIB_LIBCPP_INCLUDE_ARRAY_HPP_
 
-#include "./types.hpp"
+#include <libzig/libzig.hpp>
 
-template <class T, tedit::types::size_t N>
+template <class T, libzig::size_t N>
 struct Array final {
     T data_[N];
 
     template <class Self>
-    constexpr T& operator[](this Self&& self, tedit::types::size_t i) {
+    constexpr decltype(auto) operator[](this Self&& self, libzig::size_t i) {
         return self.data_[i];
     }
 
-    constexpr tedit::types::size_t Size() const { return N; }
+    constexpr libzig::size_t Size() const { return N; }
 
-    template <class Self>
-    constexpr auto begin(this Self&& self) {
-        return self.data_;
-    }
+    constexpr auto begin(this auto&& self) { return self.data_; }
 
-    template <class Self>
-    constexpr auto end(this Self&& self) {
-        return self.data_ + N;
-    }
+    constexpr auto end(this auto&& self) { return self.data_ + N; }
 };
 
-#endif  // SRC_INCLUDE_ARRAY_HPP_
+#endif  // LIB_LIBCPP_INCLUDE_ARRAY_HPP_
